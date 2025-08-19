bf870088b1e26f2f9ee3e2e842cac12a65fc4f08
Omar Meza <omeza3.14@hotmail.com>
2025-08-19 04:24:04 +0000

    Un commit cualquiera

--- github/views.py
+++ github/views.py
@@ -8,8 +8,10 @@ def github_webhook(request):
     if request.method == 'POST':
         # Ejecutar el script git pull
         try:
-            subprocess.run(['/home/omeza/online-shop/git_pull/pull.sh'], check=True)
+            subprocess.run(["bash","/home/omeza/online-shop/git_pull/pull.sh"], check=True)
             return HttpResponse('OK', status=200)
+        except Exception as e:
+            return HttpResponse(f'Error al ejecutar el pull: {e}', status=404)
         except subprocess.CalledProcessError as e:
             return HttpResponse(f'Error al ejecutar el pull: {e}', status=500)
     return HttpResponse('Solo se permiten solicitudes POST', status=405)
