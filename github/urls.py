16c3d2b2d7ad8f63f56bfabd19b6d7b90a11cf73
Omar Meza <omeza3.14@hotmail.com>
2025-08-18 21:57:05 -0500

    Se modifico github/urls.py

--- github/urls.py
+++ github/urls.py
@@ -4,6 +4,4 @@ from . import views  # O la ruta correcta a tu archivo views.py
 urlpatterns = [
     # Tus otras URLs...
     path('/webhook/github/', views.github_webhook, name='github_webhook'),
-
-    
 ]
