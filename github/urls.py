9fd76ca756c0c4f30fa53d3a93401f6dc1cf4665
Omar Meza <omeza3.14@hotmail.com>
2025-08-18 21:09:41 -0500

    Se agrego webhook

---
+++ github/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views  # O la ruta correcta a tu archivo views.py
+
+urlpatterns = [
+    # Tus otras URLs...
+    path('webhook/github/', views.github_webhook, name='github_webhook'),
+]
---
+++ github/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import subprocess
+
+
+@csrf_exempt
+def github_webhook(request):
+    if request.method == 'POST':
+        # Ejecutar el script git pull
+        try:
+            subprocess.run(['/git_pull/pull.sh'], check=True)
+            return HttpResponse('OK', status=200)
+        except subprocess.CalledProcessError as e:
+            return HttpResponse(f'Error al ejecutar el pull: {e}', status=500)
+    return HttpResponse('Solo se permiten solicitudes POST', status=405)
