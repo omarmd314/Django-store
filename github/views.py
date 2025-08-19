3c133f45231faaa860a010f1be64fd8fecec728f
Omar Meza <omeza3.14@hotmail.com>
2025-08-18 21:54:29 -0500

    Se modifico github/views.py

--- github/views.py
+++ github/views.py
@@ -8,7 +8,7 @@ def github_webhook(request):
     if request.method == 'POST':
         # Ejecutar el script git pull
         try:
-            subprocess.run(['/git_pull/pull.sh'], check=True)
+            subprocess.run(['/home/omeza/online-shop/git_pull/pull.sh'], check=True)
             return HttpResponse('OK', status=200)
         except subprocess.CalledProcessError as e:
             return HttpResponse(f'Error al ejecutar el pull: {e}', status=500)
